f35fe6a329125bc7bd334cbc6896d9bda04d6a8c
Thelaxman <gusainlakshman@gmail.com>
2024-08-09 10:54:01 -0600

    added update-password functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -225,4 +225,22 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     );
 });
 
+const updatePassword = asyncHandler(async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+
+  const user = await User.findById(req.user?.id);
+  const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+
+  if (!isPasswordCorrect) {
+    throw new ApiError(400, " Invalid old password");
+  }
+
+  user.password = newPassword;
+  await user.save({ validateBeforeSave: false });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password updated suuccessfully"));
+});
+
 export { registerUser, loginUser, logoutUser, refreshAccessToken };

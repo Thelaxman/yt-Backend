ecf9f4eae9faa80acdb1f372079cd642e12f56ee
Thelaxman <gusainlakshman@gmail.com>
2024-08-11 10:51:57 -0600

    routes updates

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,11 @@ import {
   logoutUser,
   registerUser,
   refreshAccessToken,
+  updatePassword,
+  updateAccountDetails,
+  updateUserAvatar,
+  getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyjwt } from "../middlewares/auth.middleware.js";
@@ -33,5 +38,16 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(verifyjwt, logoutUser);
 
 router.route("/refresh-token").post(refreshAccessToken);
+
+router.route("/change-password").post(verifyjwt, updatePassword);
+router.route("/update-info").post(verifyjwt, updateAccountDetails);
+router
+  .route("/avatar")
+
+  .patch(verifyjwt, upload.single("avatar"), updateUserAvatar);
+
+router.route("/c/:username").get(verifyjwt, getUserChannelProfile);
+
+router.route("/history").get(verifyjwt, getWatchHistory);
 
 export default router;
